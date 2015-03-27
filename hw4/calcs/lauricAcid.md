672b4f5020d500296632e910a2e982dc0092e61c
prtkm <prateekm@cmu.edu>
2015-03-27 16:24:58 -0400

    hw4

---
+++ hw4/calcs/lauricAcid.md
@@ -0,0 +1,34 @@
+molecule {
+  name = "lauricAcid";
+  atom[0] { type = "CH3"; position( 12.5502, 6.93698, 18.1241);}
+  atom[1] { type = "CH2"; position( 12.1955, 7.42403, 19.5199);}
+  atom[2] { type = "CH2"; position( 12.7327, 6.48077, 20.5961);}
+  atom[3] { type = "CH2"; position( 12.3731, 6.97551, 21.9979);}
+  atom[4] { type = "CH2"; position( 12.9096, 6.03333, 23.077);}
+  atom[5] { type = "CH2"; position( 12.5479, 6.53163, 24.4771);}
+  atom[6] { type = "CH2"; position( 13.082, 5.5929, 25.5602);}
+  atom[7] { type = "CH2"; position( 12.7171, 6.09656, 26.9576);}
+  atom[8] { type = "CH2"; position( 13.2485, 5.16209, 28.0457);}
+  atom[9] { type = "CH2"; position( 12.8804, 5.67099, 29.4406);}
+  atom[10] { type = "CH2C"; position( 13.4095, 4.7401, 30.5312);}
+  atom[11] { type = "C2"; position( 13.0877, 5.17302, 31.9381);}
+  atom[12] { type = "O2"; position( 13.4187, 4.56954, 32.9472);}
+  atom[13] { type = "OH"; position( 12.3835, 6.31216, 32.0383);}
+  atom[14] { type = "HO"; position( 12.2693, 6.43572, 33.0039);}
+
+  bond { members(0, 1); }
+  bond { members(1, 2); }
+  bond { members(2, 3); }
+  bond { members(3, 4); }
+  bond { members(4, 5); }
+  bond { members(5, 6); }
+  bond { members(6, 7); }
+  bond { members(7, 8); }
+  bond { members(8, 9); }
+  bond { members(9, 10); }
+  bond { members(10, 11); }
+  bond { members(11, 13); }
+  bond { members(11, 12); }
+  bond { members(13, 14); }
+
+}
---
+++ hw4/openmd_tools.py
@@ -0,0 +1,80 @@
+
+import numpy as np
+import itertools
+import subprocess
+import matplotlib.pyplot as plt
+import os
+
+def get_all_pairs(atomtypes):
+    '''
+    Returns a list of all possible pairs from a list of atomic speicies.
+    '''
+    return list(itertools.combinations_with_replacement(atomtypes, 2))
+
+def calculate_pair_corr(filepath, atom1, atom2):
+    '''
+    Convenience function to provide for fast calculations of pair correlation functions
+    Strangely, I could not get StaticProps to dump to a specified folder/file, 
+    so we have to take care to change directories and return data
+    '''
+    try:
+        # If a path is spefified
+        wd, filename = filepath.rsplit('.')[0].split('/')
+    except:
+        # If only filename is specified
+        filename = filepath.rsplit('.')
+        wd = '.'
+        
+    cmd = 'StaticProps -i {2}.dump -g --sele1="select {0}" --sele2="select {1}" '
+    cmd = cmd.format(atom1, atom2, filename)
+    cwd = os.getcwd()
+    os.chdir(wd)
+    subprocess.call(cmd, shell=True)
+    os.chdir(cwd)
+    return read_pair_corr('{0}/{1}.gofr'.format(wd, filename))
+
+def read_pair_corr(filepath):
+    '''Reads the pair correlation function from a gofr file'''
+    return np.loadtxt(filepath, skiprows=3, unpack=True)
+
+def read_msd(filepath):
+   '''Reads the mean square displacement from the rcorr file'''
+   return np.loadtxt(filepath, skiprows=4, unpack=True)
+
+def read_vel_corr(filepath):
+    '''Reads the velocity correlation function from the vcorr file'''
+    return np.loadtxt(filepath, skiprows=4, unpack=True)
+
+def read_power_spectrum(filepath):
+    '''
+    Reads the power spectrum from a pspect file
+    '''    
+    return np.loadtxt(filepath, unpack=True)
+   
+def msd_slope_and_interval(t, msd, tstart=0):
+    '''
+    Linear regression fitting for MSD data
+    '''
+    t_stack = np.column_stack([t[tstart:]**1, t[tstart:]**0])
+    b, res, rank, s = np.linalg.lstsq(t_stack, msd)
+    slope, interval = b
+    return slope, interval
+
+def get_D_einstien(t, msd, tstart=0, d=3):
+    '''Diffusion coefficient from Einstien relation'''
+    slope, interval = msd_slope_and_interval(t, msd, tstart) 
+    return slope / 2. / d
+
+def get_D_greenkubo(t, vcorr):
+    '''
+    Diffusion coefficient from Green-Kubo
+    '''
+    return np.abs(np.trapz(vcorr, t) * 1/ 3.)
+
+def read_charge_density(filepath):
+    '''
+    Read the charge density from the .density filename
+    '''
+    zs, density = np.loadtxt(filepath, unpack=True, skiprows=4)
+ 
+    return zs, density
